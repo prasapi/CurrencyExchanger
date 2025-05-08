e1e542b02ff372639fe0882b209bcecedea7c0b8
Prasad Pidaparthy <prasad.btechcs@gmail.com>
2025-05-08 06:48:57 +0000

    updated call_llm function

--- currencyexchanger.py
+++ currencyexchanger.py
@@ -3,27 +3,63 @@ import dotenv
 import os
 import requests
 from dotenv import load_dotenv
+import json
+import streamlit as st
+from openai import OpenAI
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.github.ai/inference"
+model_name = "openai/gpt-4o-mini"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
 
 load_dotenv()
 excr_key =os.getenv('EXCHANGERATE_API_KEY')
 
+
+
+
+
 def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
-    res = requests.get('https://v6.exchangerate-api.com/v6/excr_key/pair/EUR/GBP')
-    if res.status =='success':
-        """Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
+    url = f"https://v6.exchangerate-api.com/v6/{excr_key}/pair/{base}/{target}/{amount}"
+    res = requests.get(url)
+    resjson = None
+    if res.status_code ==200:
+        resjson = json.loads(res.text)
+        #"""Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
     else:
-        print(res.status)
+        print(res.content)
+    return (base,target,amount,excr_key,f'{resjson["conversion_result"]:.2f}')
     pass
 
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
     try:
-        completion = ...
+        response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": textbox_input,
+        }
+    ],
+    temperature=1.0,
+    top_p=1.0,
+    max_tokens=1000,
+    model=model_name
+    )
     except Exception as e:
         print(f"Exception {e} for {text}")
     else:
-        return completion
+        print(response.choices[0].message.content)
+        return response.choices[0].message.content
 
 def run_pipeline():
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
@@ -38,3 +74,16 @@ def run_pipeline():
         st.write(f"(Function calling not used) and response from the model")
     else:
         st.write("NotImplemented")
+
+print(get_exchange_rate('EUR','INR','252.4'))
+# Setting the title of the web application
+st.title("Multi-Lingual Currency  Exchanger")
+
+# Creating a text input box
+user_input = st.text_area("Enter the currency and amount")
+
+# A submit button
+if st.button("Submit"):
+    # Display the user's input below the text box
+    st.subheader("calling LLM:")
+    st.write(call_llm(user_input))
